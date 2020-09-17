be63532817b3088412c749e935f67318beceef1d
Parag Adhau <adhauparag64@gmail.com>
2020-09-17 17:19:23 +0530

    Added :Read person details from user

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -4,9 +4,27 @@ namespace AddressBook
 {
     class Program
     {
+       
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Welcome To AddressBook Management");
+            //String fname, lname, address, city, state, zip, phone;
+            Console.WriteLine("Enter First Name");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter Address");
+            string address = Console.ReadLine();
+            Console.WriteLine("Enter City");
+            string city = Console.ReadLine();
+            Console.WriteLine("Enter State");
+            string state = Console.ReadLine();
+            Console.WriteLine("Enter Zip");
+            string zip = Console.ReadLine();
+            Console.WriteLine("Enter phone number");
+            string phoneNumber = Console.ReadLine();
+
         }
     }
 }
