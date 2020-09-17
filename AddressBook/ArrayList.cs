56e4770fd52df22b59e2bd657beca19968fa38c0
Parag Adhau <adhauparag64@gmail.com>
2020-09-17 18:52:18 +0530

    Added :Add new person functionality to addressbook

---
+++ AddressBook/ArrayList.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+namespace AddressBook
+{
+    internal class ArrayList<T> : List<Person>
+    {
+    }
+}
---
+++ AddressBook/Helper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace AddressBook
+{
+    class Helper
+    {
+
+
+        public void addRecord()
+        {
+            String fname, lname, address, city, state, phone, zip;
+            Console.WriteLine("Enter First Name");
+            fname = Console.ReadLine();
+            Console.WriteLine("Enter Last Name");
+            lname = Console.ReadLine();
+            Console.WriteLine("Enter Address");
+            address = Console.ReadLine();
+            Console.WriteLine("Enter City");
+            city = Console.ReadLine();
+            Console.WriteLine("Enter State");
+            state = Console.ReadLine();
+            Console.WriteLine("Enter Zip");
+            zip = Console.ReadLine();
+            Console.WriteLine("Enter phone number");
+            phone = Console.ReadLine();
+            List<Person> person = new ArrayList<Person>();
+            person.Add(new Person(fname, lname, address, city, state, phone, zip));
+        }  
+           
+        }
+    }
+
